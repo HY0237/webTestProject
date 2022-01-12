3d02cf952078b15aa1f14564a438f0f4be715140
HyeYeong <hy0237@uab.edu>
2022-01-13 03:44:54 +0900

    Oauth으로 구글 네이버 로그인

--- src/main/java/com/web/springboot/controller/IndexController.java
+++ src/main/java/com/web/springboot/controller/IndexController.java
@@ -1,6 +1,8 @@
 package com.web.springboot.controller;
 
 
+import com.web.springboot.config.auth.LoginUser;
+import com.web.springboot.config.auth.dto.SessionUser;
 import com.web.springboot.dto.PostsResponseDto;
 import com.web.springboot.service.posts.PostsService;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +11,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
 
+
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
+
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
--- src/test/java/com/web/springboot/controller/HelloControllerTest.java
+++ src/test/java/com/web/springboot/controller/HelloControllerTest.java
@@ -1,10 +1,14 @@
 package com.web.springboot.controller;
 
 
+import com.web.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,12 +18,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class, secure = false)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello_test() throws Exception{
         String hello = "hello";
@@ -29,6 +38,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto_test() throws Exception{
         String name ="hello";
